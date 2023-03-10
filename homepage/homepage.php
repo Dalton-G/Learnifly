29f897e57a1590a8c8fed5c665c50107e9ef5bfb
Dalton-G <study.daltongan@gmail.com>
2023-03-10 23:51:03 +0800

    removed clutter

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -2,11 +2,6 @@
 
 <!-- your content goes here -->
 <!-- your content goes here -->
-<!-- your content goes here -->
-<!-- cup plate -->
-<!-- your content goes here -->
-<!-- your content goes here -->
-<!-- your content goes here -->
 
 
 <?php include "../../Learnifly/navbar/footer.php"; ?>
