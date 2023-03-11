9f7fdddba45c3b70bd3300a82273fbd51c713aee
Dalton-G <study.daltongan@gmail.com>
2023-03-11 13:02:41 +0800

    cleaned up homepage.php, removed redundant comment

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -1,9 +1,5 @@
 <?php include "../../Learnifly/navbar/header.php"; ?>
 
-
-
-<!-- adding a code -->
 <!-- you can add your content here -->
-<!-- new line :) -->
 
 <?php include "../../Learnifly/navbar/footer.php"; ?>
