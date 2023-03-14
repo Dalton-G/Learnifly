553237452acc2fc1d48555f47050227deb7b40bd
Dalton-G <study.daltongan@gmail.com>
2023-03-14 15:42:30 +0800

    change admin name to JUSTIN GILBERT! :D

--- SQL/learnifly.sql
+++ SQL/learnifly.sql
@@ -206,7 +206,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `user_name`, `user_password`, `user_email`, `user_role`, `user_profile_pic`, `user_bio`, `user_restriction`) VALUES
 (1, 'Bill Clinton', '11111111', 'bill@gmail.com', 'student', 'directory', 'I love dogs and paw-litics!', NULL),
-(2, 'admin', '12345678', 'admin@learnifly.com', 'admin', 'directory', 'All I do is testing', NULL);
+(2, 'Justin Gilbert', '12345678', 'admin@learnifly.com', 'admin', 'directory', 'All I do is testing', NULL);
 
 --
 -- Indexes for dumped tables
