cdd90ea1d3dc88f99e9f9c1e2b8bf99f27d92356
Dalton-G <study.daltongan@gmail.com>
2023-03-18 17:13:25 +0800

    remove chat from header and footer

--- navbar/header.php
+++ navbar/header.php
@@ -65,38 +65,21 @@
         </div>
         <!-- coloum 3 - right -->
         <div class="flexbox-item flexbox-item-3">
-            <!-- [3.1] chat dropdown menu -->
-            <div class="dropdown" data-dropdown>
-                <button class="navbar-link"><i class="fa-solid fa-comment" data-dropdown-button></i></button>
-                <div class="dropdown-menu information-grid">
-                    <!-- chat dropdown list start -->
-                    <div>
-                        <div class="dropdown-heading">Chat</div>
-                        <div class="dropdown-links">
-                            <a href="#" class="dropdown-navbar-link">Send Messages</a>
-                            <a href="#" class="dropdown-navbar-link">Inbox</a>
-                        </div>
-                    </div>
-                    <!-- chat dropdown list ends -->
-                </div>
-            </div>
-            <!-- [3.2] profile dropdown menu -->
+            <!-- [3.1] profile dropdown menu -->
             <div class="dropdown" data-dropdown>
                 <button class="navbar-link"><i class="fa-solid fa-user" data-dropdown-button></i></button>
                 <div class="dropdown-menu information-grid">
                     <!-- profile dropdown list start -->
                     <div>
-                        <div class="dropdown-heading">Profile</div>
+                        <div class="dropdown-heading">Account</div>
                         <div class="dropdown-links">
-                            <a href="#" class="dropdown-navbar-link">Account Settings</a>
-                            <a href="#" class="dropdown-navbar-link">Wallet</a>
                             <a href="#" class="dropdown-navbar-link">Log Out</a>
                         </div>
                     </div>
                     <!-- profile dropdown list ends -->
                 </div>
             </div>
-            <!-- [3.3] toggle theme function -->
+            <!-- [3.2] toggle theme function -->
             <div class="toggle-container">
                 <input type="checkbox" class="header-toggle-theme-checkbox-class" id="header-toggle-theme-checkbox-id" onclick="isChecked()">
                 <label for="header-toggle-theme-checkbox-id" class="header-toggle-theme-label-class">
