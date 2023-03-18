e9c1815b35c2593ebcd3769f600123f4363a4c68
BryanStudy <study.kitbryan@gmail.com>
2023-03-19 00:03:21 +0800

    Fixed some bugs, added comments

--- login/login.php
+++ login/login.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Learnifly icon -->
     <link rel="shortcut icon" href="../images/tp063338/logo/favicon.ico" type="image/x-icon">
+    <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
     <!-- Link to CSS -->
     <link rel="stylesheet" href="styles.css" />
