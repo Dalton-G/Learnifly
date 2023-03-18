d5fdcf7345c0710eee87a878acafccdb82d1e0b3
FireBuckets365 <zijin@hotmail.my>
2023-03-18 21:13:22 +0800

    modifyCourse.php deleted

--- study/course/modifyCourse.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include ("../../../Learnifly/dbConnection/dbConnection.php");
-    include ("../../../Learnifly/navbar/header.php");
-?>
-<h1>Modify Course</h1>
-<?php
-    include ("../../../Learnifly/navbar/footer.php");
-?>
